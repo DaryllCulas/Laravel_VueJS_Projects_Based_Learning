4106033ee6640febc121946366a98c2a19ccbe4c
DaryllCulas <culasdaryll99@gmail.com>
2024-09-14 03:00:18 +0800

    Inertia Form Helper

--- first_laravel_inertia_vue_project/routes/web.php
+++ first_laravel_inertia_vue_project/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Home')->name('home');
+
 Route::inertia('/register', 'Auth/Register')->name('register');
+ROute::post('/register', [AuthController::class, 'register']);
+
 
 Route::inertia('/about', 'About', ['user' => 'Nigga'])->name('about');
