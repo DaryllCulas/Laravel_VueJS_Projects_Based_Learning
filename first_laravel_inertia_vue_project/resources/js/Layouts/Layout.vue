1d9f8e0688ff5259bd821f21a55799fcce3c5317
DaryllCulas <culasdaryll99@gmail.com>
2024-09-15 03:27:21 +0800

    logout with Post links && apply middleware groups for guest and authenticated users

--- first_laravel_inertia_vue_project/resources/js/Layouts/Layout.vue
+++ first_laravel_inertia_vue_project/resources/js/Layouts/Layout.vue
@@ -1,25 +1,64 @@
 <script setup></script>
 <template>
-    <div>
-        <header>
-            <nav>
-                <Link :href="route('home')">
-                <!-- <h1 class="text-white nav-link">{{ $page.props.auth.user.name }}</h1> -->
-                </Link>
+  <div>
+    <header>
+      <nav>
+        <Link :href="route('home')">
+          <h1 v-if="$page.props.auth.user" class="text-white nav-link">
+            {{ $page.props.auth.user.name }}
+          </h1>
+        </Link>
 
-                <div class="space-x-6">
-                    <Link :href="route('home')" class="nav-link">Home</Link>
-                    <Link :href="route('about')" class="nav-link">About</Link>
-                </div>
+        <div class="space-x-6">
+          <Link
+            :href="route('home')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Home' }"
+            >Home</Link
+          >
+          <Link
+            :href="route('about')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'About' }"
+            >About</Link
+          >
+        </div>
 
-                <div class="space-x-6">
-                    <Link :href="route('register')" class="nav-link">Register</Link>
-                    <Link :href="route('login')" class="nav-link">Login</Link>
-                </div>
-            </nav>
-        </header>
-        <main>
-            <slot />
-        </main>
-    </div>
+        <div v-if="$page.props.auth.user" class="space-x-6">
+          <Link
+            :href="route('dashboard')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Dashboard' }"
+            >Dashboard</Link
+          >
+          <Link
+            :href="route('logout')"
+            class="nav-link"
+            method="post"
+            as="button"
+            type="button"
+            >Logout</Link
+          >
+        </div>
+
+        <div v-else class="space-x-6">
+          <Link
+            :href="route('register')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Auth/Register' }"
+            >Register</Link
+          >
+          <Link
+            :href="route('login')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Auth/Login' }"
+            >Login</Link
+          >
+        </div>
+      </nav>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
 </template>
--- first_laravel_inertia_vue_project/routes/web.php
+++ first_laravel_inertia_vue_project/routes/web.php
@@ -5,11 +5,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Home')->name('home');
 
-Route::inertia('/register', 'Auth/Register')->name('register');
-ROute::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth')->group(function () {
+    Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
 
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::inertia('/about', 'About', ['user' => 'Nigga'])->name('about');
+});
 
-Route::inertia('login', 'Auth/Login')->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function () {
+    Route::inertia('/register', 'Auth/Register')->name('register');
+    ROute::post('/register', [AuthController::class, 'register']);
+
+    Route::inertia('/about', 'About', ['user' => 'Nigga'])->name('about');
+
+    Route::inertia('login', 'Auth/Login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
