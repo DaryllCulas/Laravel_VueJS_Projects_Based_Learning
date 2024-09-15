1174c519756fb4098bd4f7222a0f3310aa5c819c
DaryllCulas <culasdaryll99@gmail.com>
2024-09-16 02:58:02 +0800

    Upload files

--- first_laravel_inertia_vue_project/resources/js/Layouts/Layout.vue
+++ first_laravel_inertia_vue_project/resources/js/Layouts/Layout.vue
@@ -24,7 +24,15 @@
           >
         </div>
 
-        <div v-if="$page.props.auth.user" class="space-x-6">
+        <div v-if="$page.props.auth.user" class="space-x-6 flex">
+          <img
+            class="avatar"
+            :src="
+              $page.props.auth.user.avatar
+                ? 'storage/' + $page.props.auth.user.avatar
+                : 'storage/avatars/default.png'
+            "
+          />
           <Link
             :href="route('dashboard')"
             class="nav-link"
