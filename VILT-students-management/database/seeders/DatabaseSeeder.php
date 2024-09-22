86354a9fbe6c319380ea880450ec10c579af9860
DaryllCulas <culasdaryll99@gmail.com>
2024-09-22 21:09:37 +0800

    Modified#2: Migrations, Factories, Relationships and Seeders

--- VILT-students-management/database/seeders/DatabaseSeeder.php
+++ VILT-students-management/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\ClassesSeeder;
+use Database\Seeders\UsersTableSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+      $this->call([
+        ClassesSeeder::class,
+        UsersTableSeeder::class
+      ]);
     }
 }
