a7a10e36d116fd727e2056a38a3723bff6f0a0a4
DaryllCulas <culasdaryll99@gmail.com>
2024-10-06 04:24:55 +0800

    2 create Role and Admin Seeder

--- Laravel-inertia-roles-permission/routes/web.php
+++ Laravel-inertia-roles-permission/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -23,5 +24,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.edit');
+});
+
+
 
 require __DIR__.'/auth.php';
