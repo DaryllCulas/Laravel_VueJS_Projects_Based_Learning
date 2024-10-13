dcd6e9e09f7e578a5c31e7bcc5cf13584248567b
DaryllCulas <culasdaryll99@gmail.com>
2024-10-14 04:23:56 +0800

    Confirmation Delete Modal for Roles and Permissions

--- Laravel-inertia-roles-permission/resources/js/Pages/Admin/Roles/RoleIndex.vue
+++ Laravel-inertia-roles-permission/resources/js/Pages/Admin/Roles/RoleIndex.vue
@@ -1,12 +1,34 @@
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import { Head, Link } from "@inertiajs/vue3";
+import { Head, Link, useForm } from "@inertiajs/vue3";
+import { ref } from "vue";
 import Table from "@/Components/Table.vue";
 import TableRow from "@/Components/TableRow.vue";
 import TableHeaderCell from "@/Components/TableHeaderCell.vue";
 import TableDataCell from "@/Components/TableDataCell.vue";
+import Modal from "@/Components/Modal.vue";
+import DangerButton from "@/Components/DangerButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 defineProps(["roles"]);
+const form = useForm({});
+
+const showConfirmDeleteRoleModal = ref(false);
+
+const confirmDeleteRole = () => {
+  showConfirmDeleteRoleModal.value = true;
+};
+
+const closeModal = () => {
+  showConfirmDeleteRoleModal.value = false;
+};
+
+const deleteRole = (id) => {
+  form.delete(route("roles.destroy", id), {
+    onSuccess: () => closeModal(),
+    preserveScroll: true,
+  });
+};
 </script>
 
 <template>
@@ -41,13 +63,25 @@ defineProps(["roles"]);
                   class="text-green-400 hover:text-green-600"
                   >Edit</Link
                 >
-                <Link
-                  :href="route('roles.destroy', role.id)"
-                  method="DELETE"
-                  as="button"
+                <button
+                  @click="confirmDeleteRole"
                   class="text-red-400 hover:text-red-600"
-                  >Delete</Link
                 >
+                  Delete
+                </button>
+                <Modal :show="showConfirmDeleteRoleModal" @close="closeModal">
+                  <div class="p-6">
+                    <h2 class="text-lg font-semibold text-slate-800">
+                      Are you sure you want to delete this user?
+                    </h2>
+                    <div class="mt-6 flex space-x-4">
+                      <DangerButton @click="($event) => deleteRole(role.id)"
+                        >Delete</DangerButton
+                      >
+                      <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+                    </div>
+                  </div>
+                </Modal>
               </TableDataCell>
             </TableRow>
           </template>
