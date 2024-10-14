cbe2e792e352ad57be4fc17fc5df28cef4b81f14
DaryllCulas <culasdaryll99@gmail.com>
2024-10-15 02:49:39 +0800

    Protect Policy with Post policy using Gate authorization

--- Laravel-inertia-roles-permission/app/Http/Controllers/PostController.php
+++ Laravel-inertia-roles-permission/app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\CreatePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -22,11 +23,13 @@ class PostController extends Controller
 
     public function create(): Response
     {
+        Gate::authorize('create', Post::class);
         return Inertia::render('Admin/Posts/Create');
     }
 
     public function store(CreatePostRequest $request): RedirectResponse
     {
+        Gate::authorize('create', Post::class);
         Post::create($request->validated());
 
         return to_route('posts.index');
@@ -34,6 +37,8 @@ class PostController extends Controller
 
     public function edit(Post $post): Response
     {
+        Gate::authorize('update', $post);
+
         return Inertia::render('Admin/Posts/Edit', [
             'post' => new PostResource($post)
         ]);
@@ -41,6 +46,8 @@ class PostController extends Controller
 
     public function update(CreatePostRequest $request, Post $post): RedirectResponse
     {
+        Gate::authorize('update', $post);
+
         $post->update($request->validated());
 
         return to_route('posts.index');
@@ -48,6 +55,8 @@ class PostController extends Controller
 
     public function destroy(Post $post): RedirectResponse
     {
+        Gate::authorize('delete', $post);
+
         $post->delete();
         return back();
     }
