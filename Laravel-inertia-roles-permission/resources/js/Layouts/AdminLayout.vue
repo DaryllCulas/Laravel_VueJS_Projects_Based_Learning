bb116fbbc18ae3d8510b93dba73442e7767da9b1
DaryllCulas <culasdaryll99@gmail.com>
2024-10-15 02:07:19 +0800

    Show links based on Permission

--- Laravel-inertia-roles-permission/resources/js/Layouts/AdminLayout.vue
+++ Laravel-inertia-roles-permission/resources/js/Layouts/AdminLayout.vue
@@ -18,13 +18,7 @@ const showingNavigationDropdown = ref(false);
     >
       <div>
         <div class="-mx-6 px-6 py-4">
-          <a href="#" title="home">
-            <img
-              src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
-              class="w-32"
-              alt="tailus logo"
-            />
-          </a>
+          {{ $page.props.auth.user.name }}
         </div>
 
         <ul class="space-y-2 tracking-wide mt-8">
--- Laravel-inertia-roles-permission/resources/js/Pages/Admin/Posts/PostIndex.vue
+++ Laravel-inertia-roles-permission/resources/js/Pages/Admin/Posts/PostIndex.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
+import { usePermission } from "@/composables/permissions";
 import { ref } from "vue";
 import Table from "@/Components/Table.vue";
 import TableRow from "@/Components/TableRow.vue";
@@ -14,6 +15,7 @@ defineProps(["posts"]);
 const form = useForm({});
 
 const showConfirmDeletePostModal = ref(false);
+const { hasPermission } = usePermission();
 
 const confirmDeletePost = () => {
   showConfirmDeletePostModal.value = true;
@@ -38,11 +40,13 @@ const deletePost = (id) => {
     <div class="max-w-7xl mx-auto py-4">
       <div class="flex justify-between">
         <h1>Posts Index Page</h1>
-        <Link
-          :href="route('posts.create')"
-          class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded"
-          >New Post</Link
-        >
+        <template v-if="hasPermission('create post')">
+          <Link
+            :href="route('posts.create')"
+            class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded"
+            >New Post</Link
+          >
+        </template>
       </div>
       <div class="mt-6">
         <Table>
@@ -58,17 +62,21 @@ const deletePost = (id) => {
               <TableDataCell>{{ post.id }}</TableDataCell>
               <TableDataCell>{{ post.title }}</TableDataCell>
               <TableDataCell class="space-x-6">
-                <Link
-                  :href="route('posts.edit', post.id)"
-                  class="text-green-400 hover:text-green-600"
-                  >Edit</Link
-                >
-                <button
-                  @click="confirmDeletePost"
-                  class="text-red-400 hover:text-red-600"
-                >
-                  Delete
-                </button>
+                <template v-if="hasPermission('update post')">
+                  <Link
+                    :href="route('posts.edit', post.id)"
+                    class="text-green-400 hover:text-green-600"
+                    >Edit</Link
+                  >
+                </template>
+                <template v-if="hasPermission('delete post')">
+                  <button
+                    @click="confirmDeletePost"
+                    class="text-red-400 hover:text-red-600"
+                  >
+                    Delete
+                  </button>
+                </template>
                 <Modal :show="showConfirmDeletePostModal" @close="closeModal">
                   <div class="p-6">
                     <h2 class="text-lg font-semibold text-slate-800">
